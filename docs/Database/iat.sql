cf2e067f61d6ea51cb6152f7d1f5e7408fc040f0
TheCourierNV <andreascart04@gmail.com>
2023-05-22 12:00:13 +0200

    Aggiornato schema del database

--- docs/Database/iat.sql
+++ docs/Database/iat.sql
@@ -1,36 +1,67 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.5.19-MariaDB, for debian-linux-gnu (aarch64)
 --
--- Host: localhost
--- Creato il: Mag 10, 2023 alle 10:29
--- Versione del server: 10.4.28-MariaDB
--- Versione PHP: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: iat
+-- ------------------------------------------------------
+-- Server version	10.5.19-MariaDB-0+deb11u2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `iat`
---
-CREATE DATABASE IF NOT EXISTS `iat` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `iat`;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `eventi`
+-- Table structure for table `Users`
 --
 
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `users_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  `hashed_password` varchar(256) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `stringRetrievePassword` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`users_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Users_tokens`
+--
+
+DROP TABLE IF EXISTS `Users_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `selector` varchar(255) NOT NULL,
+  `hashed_validator` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `expiry` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `User_UserTokens` (`user_id`),
+  CONSTRAINT `User_UserTokens` FOREIGN KEY (`user_id`) REFERENCES `Users` (`users_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Used to implement remember me in login.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `eventi`
+--
+
+DROP TABLE IF EXISTS `eventi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `eventi` (
-  `id_evento` int(11) NOT NULL,
+  `id_evento` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) DEFAULT NULL,
   `data_inizio` datetime DEFAULT NULL,
   `data_fine` datetime DEFAULT NULL,
@@ -39,112 +70,73 @@ CREATE TABLE `eventi` (
   `localita` varchar(100) DEFAULT NULL,
   `telefono` int(10) NOT NULL,
   `longitudine` double NOT NULL,
-  `latitudine` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `latitudine` double NOT NULL,
+  PRIMARY KEY (`id_evento`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Struttura della tabella `itinerari`
+-- Table structure for table `itinerari`
 --
 
+DROP TABLE IF EXISTS `itinerari`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `itinerari` (
-  `id_itinerario` int(11) NOT NULL,
+  `id_itinerario` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) DEFAULT NULL,
   `descrizione` varchar(255) DEFAULT NULL,
   `tipologia` varchar(50) DEFAULT NULL,
   `localita` varchar(50) DEFAULT NULL,
   `latitudine` double NOT NULL,
-  `longitudine` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `longitudine` double NOT NULL,
+  PRIMARY KEY (`id_itinerario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Struttura della tabella `servizi`
+-- Table structure for table `ricettivita`
 --
 
+DROP TABLE IF EXISTS `ricettivita`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ricettivita` (
+  `id_ricettivita` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  `latitudine` double DEFAULT NULL,
+  `longitudine` double DEFAULT NULL,
+  `localita` varchar(50) DEFAULT NULL,
+  `tipologia` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_ricettivita`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `servizi`
+--
+
+DROP TABLE IF EXISTS `servizi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `servizi` (
-  `id_servizio` int(11) NOT NULL,
+  `id_servizio` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `latitudine` double NOT NULL,
   `longitudine` double NOT NULL,
-  `localita` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `localita` varchar(50) NOT NULL,
+  `tipologia` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_servizio`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella `Users`
---
-
-CREATE TABLE `Users` (
-  `users_id` int(11) NOT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `email` varchar(256) DEFAULT NULL,
-  `hashed_password` varchar(256) DEFAULT NULL,
-  `active` tinyint(1) DEFAULT NULL,
-  `stringRetrievePassword` varchar(256) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indici per le tabelle scaricate
---
-
---
--- Indici per le tabelle `eventi`
---
-ALTER TABLE `eventi`
-  ADD PRIMARY KEY (`id_evento`);
-
---
--- Indici per le tabelle `itinerari`
---
-ALTER TABLE `itinerari`
-  ADD PRIMARY KEY (`id_itinerario`);
-
---
--- Indici per le tabelle `servizi`
---
-ALTER TABLE `servizi`
-  ADD PRIMARY KEY (`id_servizio`);
-
---
--- Indici per le tabelle `Users`
---
-ALTER TABLE `Users`
-  ADD PRIMARY KEY (`users_id`);
-
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `eventi`
---
-ALTER TABLE `eventi`
-  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `itinerari`
---
-ALTER TABLE `itinerari`
-  MODIFY `id_itinerario` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `servizi`
---
-ALTER TABLE `servizi`
-  MODIFY `id_servizio` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `Users`
---
-ALTER TABLE `Users`
-  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-22 11:58:08
